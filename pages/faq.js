2381cbc3ed97f6bc4dd9a3b7ac8ec4b00b3f8c25
guimamura <guilherme.imamura.fernandes@gmail.com>
2023-08-05 22:35:31 -0300

    feat: first pages

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
+            <Link href="/">
+                Ir para a home
+            </Link>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Alura Cases - Home Page</h1>
+            <Link href="/faq">
+                Ir para a página do FAQ
+            </Link>
+        </div>
+    )
+}
