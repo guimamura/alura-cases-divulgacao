3fc493ae25ada87c95e5158058ed42691d2e824f
guimamura <guilherme.imamura.fernandes@gmail.com>
2023-08-05 22:45:33 -0300

    chore: added export script

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function HomePage() {
     return (
         <div>
-            <h1>Alura Cases - Home Page</h1>
+            <h1>Alura Cases - Home</h1>
             <Link href="/faq">
                 Ir para a página do FAQ
             </Link>
